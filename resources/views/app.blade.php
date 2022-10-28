888a9aa129a9456b1606b453c61a00d8003132b2
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-10-29 00:53:16 +0200

    Add Navlinks to the vue app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <title>{{ config("app.name") }}</title>
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-        <script src="{{ mix('/js/app.js') }}" defer></script>
         @inertiaHead
     </head>
     <body>
         <section class="p-8">
             @inertia
         </section>
+        <script src="{{ mix('/js/app.js') }}" defer></script>
     </body>
 </html>
