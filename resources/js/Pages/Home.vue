0cacfc9de0fc2dfc66bc0585598c68f9328b6421
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-10-28 22:46:06 +0200

    Add Pages and pass data from web.php in laravel routes to vue.js

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello</h1>
+    <Nav />
+</template>
+
+<script>
+    import Nav from '../Shared/Nav.vue';
+
+    export default {
+        components: {
+            Nav
+        },
+    };
+</script>
