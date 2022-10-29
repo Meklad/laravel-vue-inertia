787d5a6998f1ed63b475def82287e911947d21a1
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-10-29 15:03:42 +0200

    Add is active key to users table in model migration factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_active' => fake()->boolean(),
             'remember_token' => Str::random(10),
-            'stripe_token' => Str::random(10)
+            'stripe_token' => "$2y$" . Str::random(60)
         ];
     }
 
