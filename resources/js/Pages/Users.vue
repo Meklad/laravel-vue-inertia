0872b9feb0776ee55b17d474484c094f4e8c69a0
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-10-29 13:10:12 +0200

    assign default layout in app.js if not assign in other pages

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -12,11 +12,9 @@
 </template>
 
 <script>
-    import Layout from '../Shared/Layout.vue';
     import { Link } from '@inertiajs/inertia-vue3';
 
     export default {
-        layout: Layout,
         components: {
             Link
         },
