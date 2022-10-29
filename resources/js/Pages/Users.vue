aaee77117a660af6a1cc13dd6dbd5d5f7baca8ca
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-10-29 13:59:29 +0200

    Clean up the Users Page

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -7,16 +7,5 @@
     <h1 class="text-3xl">
         Users
     </h1>
-
-    <div style="margin-top: 600px">
-        <p>The Current Time is {{ time }}.</p>
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-            Refresh
-        </Link>
-    </div>
 </template>
 
-<script setup>
-    import { Link } from '@inertiajs/inertia-vue3';
-    defineProps({time: String})
-</script>
